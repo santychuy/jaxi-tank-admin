fecdc68498e26f374cf82b5754be1c25c4b34966
Santiago Carrasco <jsanticc@hotmail.com>
2020-05-21 13:31:31 -0700

    Lets begin

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
-import Document, { Main, NextScript } from 'next/document';
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -18,12 +19,19 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+            async
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Button } from 'semantic-ui-react';
 
 export default () => (
   <>
-    <Title>Hola</Title>
+    <Button>Orale</Button>
   </>
 );
-
-const Title = styled.h1`
-  color: red;
-`;
