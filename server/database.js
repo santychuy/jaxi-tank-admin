275624d1bcd3526c6fcba35b4e41068899727521
Santiago Carrasco <jsanticc@hotmail.com>
2020-05-26 14:53:05 -0700

    Changes variables DB

--- server/database.js
+++ server/database.js
@@ -2,15 +2,20 @@ import { connect } from 'mongoose';
 
 import { config } from './config';
 
-const {
+// Remove comments when its a valid URL of DB Production
+/* const {
   APP_SETTINGS: { NODE_ENV },
   DB: { URI_LOCAL, URI_PROD },
+} = config; */
+
+const {
+  DB: { URI_LOCAL },
 } = config;
 
 (async () => {
-  const uri = NODE_ENV === 'development' ? URI_LOCAL : URI_PROD;
+  // const uri = NODE_ENV === 'development' ? URI_LOCAL : URI_PROD;
   try {
-    await connect(uri, {
+    await connect(URI_LOCAL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
