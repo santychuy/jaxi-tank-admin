7aabbf8e2a04fb5c4777020414f3a10e13ccd8a4
Santiago Carrasco <jsanticc@hotmail.com>
2020-05-26 15:08:49 -0700

    Ready

--- server/database.js
+++ server/database.js
@@ -2,20 +2,15 @@ import { connect } from 'mongoose';
 
 import { config } from './config';
 
-// Remove comments when its a valid URL of DB Production
-/* const {
+const {
   APP_SETTINGS: { NODE_ENV },
   DB: { URI_LOCAL, URI_PROD },
-} = config; */
-
-const {
-  DB: { URI_LOCAL },
 } = config;
 
 (async () => {
-  // const uri = NODE_ENV === 'development' ? URI_LOCAL : URI_PROD;
+  const uri = NODE_ENV === 'development' ? URI_LOCAL : URI_PROD;
   try {
-    await connect(URI_LOCAL, {
+    await connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
